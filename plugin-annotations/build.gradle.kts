2bce5fe786371f8d790038b9a6409456e41e9553
Kevin Chiu <kevin.chiu@bandlab.com>
2025-09-23 15:51:31 +0800

    Keep only JVM and Android targets

--- plugin-annotations/build.gradle.kts
+++ plugin-annotations/build.gradle.kts
@@ -17,34 +17,7 @@ kotlin {
     androidNativeX64()
     androidNativeX86()
 
-    iosArm64()
-    iosSimulatorArm64()
-    iosX64()
-
-    js().nodejs()
-
     jvm()
-
-    linuxArm64()
-    linuxX64()
-
-    macosArm64()
-    macosX64()
-
-    mingwX64()
-
-    tvosArm64()
-    tvosSimulatorArm64()
-    tvosX64()
-
-    wasmJs().nodejs()
-    wasmWasi().nodejs()
-
-    watchosArm32()
-    watchosArm64()
-    watchosDeviceArm64()
-    watchosSimulatorArm64()
-    watchosX64()
 
     applyDefaultHierarchyTemplate()
 }
