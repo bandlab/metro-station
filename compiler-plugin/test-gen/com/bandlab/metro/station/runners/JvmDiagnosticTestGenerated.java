17ffa1e7c828ff94fcadeb8dc947309978e2b5d6
Kevin Chiu <kevin.chiu@bandlab.com>
2025-10-08 10:10:56 +0700

    Kotlin 2.3.0-Beta1
    # Conflicts:
    #	gradle/libs.versions.toml

--- compiler-plugin/test-gen/com/bandlab/metro/station/runners/JvmDiagnosticTestGenerated.java
+++ compiler-plugin/test-gen/com/bandlab/metro/station/runners/JvmDiagnosticTestGenerated.java
@@ -4,7 +4,6 @@ package com.bandlab.metro.station.runners;
 
 import com.intellij.testFramework.TestDataPath;
 import org.jetbrains.kotlin.test.util.KtTestUtil;
-import org.jetbrains.kotlin.test.TargetBackend;
 import org.jetbrains.kotlin.test.TestMetadata;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -19,7 +18,7 @@ import java.util.regex.Pattern;
 public class JvmDiagnosticTestGenerated extends AbstractJvmDiagnosticTest {
   @Test
   public void testAllFilesPresentInDiagnostics() {
-    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
+    KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics"), Pattern.compile("^(.+)\\.kt$"), null, true);
   }
 
   @Nested
@@ -28,7 +27,7 @@ public class JvmDiagnosticTestGenerated extends AbstractJvmDiagnosticTest {
   public class Metrostation {
     @Test
     public void testAllFilesPresentInMetrostation() {
-      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics/metrostation"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics/metrostation"), Pattern.compile("^(.+)\\.kt$"), null, true);
     }
 
     @Test
@@ -44,7 +43,7 @@ public class JvmDiagnosticTestGenerated extends AbstractJvmDiagnosticTest {
   public class Stationentry {
     @Test
     public void testAllFilesPresentInStationentry() {
-      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics/stationentry"), Pattern.compile("^(.+)\\.kt$"), null, TargetBackend.JVM_IR, true);
+      KtTestUtil.assertAllTestsPresentByMetadataWithExcluded(this.getClass(), new File("compiler-plugin/testData/diagnostics/stationentry"), Pattern.compile("^(.+)\\.kt$"), null, true);
     }
 
     @Test
