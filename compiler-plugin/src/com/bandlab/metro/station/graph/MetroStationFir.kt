a6430ef0f717c8bfb6491b191419610fbc76975b
Kevin Chiu <kevin.chiu@bandlab.com>
2026-06-09 16:27:04 +0800

    Remove unnecessary suppress

--- compiler-plugin/src/com/bandlab/metro/station/graph/MetroStationFir.kt
+++ compiler-plugin/src/com/bandlab/metro/station/graph/MetroStationFir.kt
@@ -69,8 +69,6 @@ public class MetroStationFir(session: FirSession, compatContext: CompatContext) 
      */
     private val componentTypeCache = mutableMapOf<ClassId, ComponentType>()
 
-    //TODO: This suppress is irrelevant after metro 1.1.2
-    @Suppress("INVISIBLE_REFERENCE")
     private val typeResolverFactory by lazy { MetroFirTypeResolver.Factory(session) }
 
     override fun FirDeclarationPredicateRegistrar.registerPredicates() {
