3400ee69f94b217f6c1996b0b775c522571b7161
Kevin Chiu <kevin.chiu@bandlab.com>
2026-07-29 10:19:38 +0800

    Lower :plugin-annotations jvm compatibility to 17

--- plugin-annotations/build.gradle.kts
+++ plugin-annotations/build.gradle.kts
@@ -16,8 +16,8 @@ kotlin {
 }
 
 java {
-    sourceCompatibility = JavaVersion.VERSION_21
-    targetCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
 }
 
 dependencies {
